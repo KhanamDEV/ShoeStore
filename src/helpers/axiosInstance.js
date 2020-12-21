9f5df4003e800e0005aa01f962258a2ac0d4e88f
KhaNam <khanamdev@gmail.con>
2020-12-21 13:09:26 +0700

    ud

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://172.17.178.1:8000/api/v1/',
+  baseURL: 'http://192.168.194.1:8000/api/v1/',
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
